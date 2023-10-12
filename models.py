f7411971fe5f65df20f005d1c5dc887370f1834e
Israelm3 <138412290+Israelm3@users.noreply.github.com>
2023-10-11 20:38:17 -0600

    [Fix]: se elimina la linea 47-50, 52-66, 65-68

--- models.py
+++ models.py
@@ -43,26 +43,3 @@ def is_approved(self):
 def __repr__(self):
      return f"Student(name='{self.name}', email='{self.email}', id='{self.id}', score='{self.score}')"
      print ([student_score1])
-
-#list1 = list()
-#for i in range(3):
-#     list1.append(str(uuid.uuid4()))
-#print(list1)
-
-list1 = [
-     '891200ac-446c-496d-b1a5-7cdb70744536', 
-     '0a526e9a-82ea-4e3b-a7df-5ab99cd78175', 
-     'f2df5d96-3d8f-4802-9279-ffabd732e79e',
-]
-
-dict1 = dict()
-for i,uuid_str in enumerate (list1):
-     dict1[uuid_str] = Student(
-          name="student" + str (i),
-          email="email" + str(i) + "@utsc.edu.mx",
-          id=uuid_str
-
-     )
-print(dict1) 
-print(dict1['f2df5d96-3d8f-4802-9279-ffabd732e79e'])
-dict2={'891200ac-446c-496d-b1a5-7cdb70744536': Student(name='student0',email='email0@utsc.edu.mx',id='891200ac-446c-496d-b1a5-7cdb70744536'), '0a526e9a-82ea-4e3b-a7df-5ab99cd78175': Student(name='student1',email='email1@utsc.edu.mx',id='0a526e9a-82ea-4e3b-a7df-5ab99cd78175'), 'f2df5d96-3d8f-4802-9279-ffabd732e79e': Student(name='student2',email='email2@utsc.edu.mx',id='f2df5d96-3d8f-4802-9279-ffabd732e79e')}
